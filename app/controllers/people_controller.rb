e065cc51002d2843de18a0687f64ea2150ceca9a
trishogen <patriciaogen@gmail.com>
2020-10-25 15:17:17 -0400

    create a person

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,15 +3,17 @@ class PeopleController < ApplicationController
   before_action :set_person, only: [:show]
 
   def index
-    # gets all the people, sends them as a json list
+    # GET all the people
     people = Person.all
 
-    render json: PersonSerializer.new(people).to_serialized_json
+    # send the list of people objects to the serializer to get a json list
+    render json: PersonSerializer.new(people).to_serialized_json, status: :ok
   end
 
   def show
-    # send the person object to the serializer to make a json object
-    if @person
+    # GET 1 person
+    if @person # uses the person set using the before_action hook
+      # send the person object to the serializer to make a json object
       render json: PersonSerializer.new(@person).to_serialized_json, status: :ok
     else
       render json: { error: "Person not found" }, status: :not_found
@@ -19,11 +21,30 @@ class PeopleController < ApplicationController
 
   end
 
+  def create
+    # POST to people
+    person = Person.new(person_params) # initialize new person with strong params
+
+    if person.save
+      # if the person was able to save successfully return json
+      render json: PersonSerializer.new(person).to_serialized_json, status: :created
+    else
+      # if the person was not able to be saved show the message from the first failed validation
+      render json: { error: person.errors.full_messages[0] }, status: :bad_request
+    end
+  end
+
   private
+
+  def person_params
+    # strong params to enforce permissable parameters
+    params.require(:person).permit(:first_name, :middle_name, :last_name,
+      :email, :age)
+  end
 
   def set_person
     # finds a given person by their ID
-    @person = Person.find_by(id: params[:id])
+    @person = Person.find(params[:id])
   end
 
 end
